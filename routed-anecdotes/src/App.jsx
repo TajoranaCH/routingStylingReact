bfa43b91387696def8f5fcdf8fbe0cfaa7971c3e
Japneet Singh <japneetsingh1807@outlook.it>
2024-07-04 12:07:49 +0200

    exc

--- routed-anecdotes/src/App.jsx
+++ routed-anecdotes/src/App.jsx
@@ -9,7 +9,9 @@ import {
   useNavigate,
   useMatch
 } from "react-router-dom"
-
+import {
+  useField
+} from './hooks'
 const Anecdote = ({ anecdote }) => {
   return (
     <div>
@@ -51,18 +53,18 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
   const navigate = useNavigate()
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     navigate('/')
@@ -74,18 +76,19 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...{...content, reset: null}} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...{...author, reset: null}} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...{...info, reset: null}} />
         </div>
         <button>create</button>
       </form>
+      <button onClick={() => {[content, author, info].map(i => i.reset());}}>reset</button>
     </div>
   )
 
